86102eecf2355808a8deea73c125f77bfa6993d7
putubagus13 <putu031216@gmail.com>
2023-07-15 12:04:01 +0700

    feat: wishlist button

--- src/screen/EvenDetail.jsx
+++ src/screen/EvenDetail.jsx
@@ -8,27 +8,74 @@ import {
 } from 'react-native';
 import React from 'react';
 import moment from 'moment';
-import {useNavigation, useRoute} from '@react-navigation/native';
+import {
+    useNavigation,
+    useRoute,
+    useFocusEffect,
+} from '@react-navigation/native';
 import http from '../helpers/http';
 import LinearGradient from 'react-native-linear-gradient';
+import Icon from 'react-native-vector-icons/Feather';
+import globalStyles from '../assets/globalStyles';
+import {useSelector} from 'react-redux';
 
 const EvenDetail = () => {
     const navigation = useNavigation();
     const route = useRoute();
     const {id} = route.params;
     const [events, setEvents] = React.useState({});
+    const token = useSelector(state => state.auth.token);
 
     const doBooking = id => {
         navigation.navigate('Booking', {id});
     };
 
-    React.useEffect(() => {
-        const getEventData = async () => {
-            const {data} = await http().get(`/events/detail/${id}`);
-            setEvents(data.results);
-        };
-        getEventData(id);
+    const getEventDetail = React.useCallback(async () => {
+        const {data} = await http().get(`/events/detail/${id}`);
+        setEvents(data.results);
     }, [id]);
+
+    useFocusEffect(
+        React.useCallback(() => {
+            const fetchEventDetail = async () => {
+                try {
+                    const {data} = await http().get(`/events/detail/${id}`);
+                    setEvents(data.results);
+                } catch (error) {
+                    const message = error?.response?.data?.message;
+                    if (message) {
+                        console.log(message);
+                    }
+                }
+            };
+            fetchEventDetail();
+        }, [id]),
+    );
+
+    React.useEffect(() => {
+        getEventDetail();
+    }, [getEventDetail]);
+
+    const addRemoveWishlist = async () => {
+        try {
+            const form = new URLSearchParams({eventId: id}).toString();
+            const {data} = await http(token).get(`/wishList/${id}`);
+            console.log(data);
+            if (data.results) {
+                const dltWishlist = await http(token).delete(`/wishList/${id}`);
+                console.log(dltWishlist);
+                getEventDetail();
+            } else if (data.message === 'wishlist not found') {
+                await http(token).post('/wishList', form);
+                getEventDetail();
+            }
+        } catch (err) {
+            const message = err?.response?.data?.message;
+            if (message) {
+                console.log(message);
+            }
+        }
+    };
     return (
         <View style={styles.wrapMain}>
             <ScrollView showsVerticalScrollIndicator={true}>
@@ -65,24 +112,59 @@ const EvenDetail = () => {
                 <View style={styles.bannerTextWrap}>
                     <Text style={styles.textTittle}>{events.title}</Text>
                     <View style={styles.wrapLocationDate}>
-                        <Text style={styles.textLocation}>
-                            {events.location}
-                        </Text>
-                        <Text style={styles.textDate}>
-                            {moment(events.date).format('MMMM Do YYYY, h:mm')}
-                        </Text>
+                        <View style={{flexDirection: 'row', gap: 10}}>
+                            <Icon
+                                style={globalStyles.colorWhite}
+                                name="map-pin"
+                                size={20}
+                            />
+                            <Text style={styles.textLocation}>
+                                {events.location}, Indonesia
+                            </Text>
+                        </View>
+                        <View style={{flexDirection: 'row', gap: 10}}>
+                            <Icon
+                                style={globalStyles.colorWhite}
+                                name="clock"
+                                size={20}
+                            />
+                            <Text style={styles.textDate}>
+                                {moment(events.date).format(
+                                    'MMMM Do YYYY, h:mm',
+                                )}
+                            </Text>
+                        </View>
                         <View style={styles.wrapAttendees}>
                             <Text style={styles.attendees}>Attendees</Text>
                             <Image source={require('../assets/Group27.png')} />
                         </View>
                     </View>
+                    <View style={{width: '100%', alignItems: 'flex-end'}}>
+                        {events.id === events.eventId ? (
+                            <TouchableOpacity
+                                onPress={() => addRemoveWishlist(events.id)}>
+                                <Icon
+                                    style={globalStyles.colorError}
+                                    name="heart"
+                                    size={22}
+                                />
+                            </TouchableOpacity>
+                        ) : (
+                            <TouchableOpacity
+                                onPress={() => addRemoveWishlist(events.id)}>
+                                <Icon
+                                    style={globalStyles.colorNeutral}
+                                    name="heart"
+                                    size={22}
+                                />
+                            </TouchableOpacity>
+                        )}
+                    </View>
                 </View>
                 <View style={styles.wrapEventDetail}>
                     <Text style={styles.textHeddingone}>Event Detail</Text>
                     <Text style={styles.descriptions}>{events.desciption}</Text>
-                </View>
-                <View style={styles.wrapEventLocation}>
-                    <Text style={styles.textHeddingtwo}>Event Location</Text>
+                    <Text style={styles.textHeddingone}>Event Location</Text>
                     <Image
                         source={require('../assets/Rectangle.png')}
                         style={styles.ImageLocation}
@@ -93,6 +175,18 @@ const EvenDetail = () => {
                         </View>
                     </TouchableOpacity>
                 </View>
+                {/* <View style={styles.wrapEventLocation}>
+                    <Text style={styles.textHeddingtwo}>Event Location</Text>
+                    <Image
+                        source={require('../assets/Rectangle.png')}
+                        style={styles.ImageLocation}
+                    />
+                    <TouchableOpacity onPress={() => doBooking(events.id)}>
+                        <View style={styles.button}>
+                            <Text style={styles.textButton}>Buy Tickets</Text>
+                        </View>
+                    </TouchableOpacity>
+                </View> */}
             </ScrollView>
         </View>
     );
@@ -111,7 +205,7 @@ const styles = StyleSheet.create({
     wrapEventDetail: {
         marginTop: -50,
         width: '100%',
-        height: 800,
+        height: '100%',
         backgroundColor: '#003d3b',
         borderTopStartRadius: 40,
         borderTopEndRadius: 40,
@@ -122,7 +216,8 @@ const styles = StyleSheet.create({
 
     bannerTextWrap: {
         position: 'absolute',
-        top: 60,
+        width: '100%',
+        top: 80,
         paddingHorizontal: 30,
         gap: 15,
     },
